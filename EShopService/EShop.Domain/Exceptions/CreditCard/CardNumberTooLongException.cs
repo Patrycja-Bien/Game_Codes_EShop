106c4cb6836f3a12d92633da393ed227108f4612
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2025-03-16 21:48:27 +0100

    Lab 3 Ex. 3

---
+++ EShopService/EShop.Domain/Exceptions/CreditCard/CardNumberTooLongException.cs
@@ -0,0 +1,9 @@
+﻿namespace EShop.Domain.Exceptions.CreditCard
+{
+    public class CardNumberTooLongException : Exception
+    {
+        public CardNumberTooLongException() : base("Card Number is too short") { }
+
+        public CardNumberTooLongException(Exception innerException) : base("Card Number is too short", innerException) { }
+    }
+}
