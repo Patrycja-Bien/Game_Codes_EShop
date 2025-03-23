e6a7046c739d6bf86b02c798e4d54597a19518ab
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2025-03-23 22:31:47 +0100

    Lab 4 Ex. 3

--- EShopService/EShop.Domain/Repositories/DataContext.cs
+++ EShopService/EShop.Domain/Repositories/DataContext.cs
@@ -5,5 +5,8 @@ namespace EShop.Domain.Repositories
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
