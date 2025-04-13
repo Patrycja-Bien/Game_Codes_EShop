1779bfa5e561fb96b3834a4f85b95a1f39763d31
Bartłomiej Balsamski <bartlomiej.balsamski@gmail.com>
2025-04-13 17:44:46 +0200

    Lab 6 Ex. 3

--- EShopService/EShopService.IntegrationTests/Controllers/ProductControllerIntegrationTest.cs
+++ EShopService/EShopService.IntegrationTests/Controllers/ProductControllerIntegrationTest.cs
@@ -97,5 +97,35 @@ namespace EShopService.IntegrationTests.Controllers
             var products = await response.Content.ReadFromJsonAsync<List<Product>>();
             Assert.Equal(10000, products?.Count);
         }
+
+        [Fact]
+        public async Task Add_AddThousandsProductsAsync_ExceptedThousandsProducts()
+        {
+            // Arrange
+            using (var scope = _factory.Services.CreateScope())
+            {
+                // Pobranie kontekstu bazy danych
+                var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+                dbContext.Products.RemoveRange(dbContext.Products);
+
+                for (int i = 0; i < 10000; i++)
+                {
+                    dbContext.Products.AddRange(
+                        new Product { Name = "Product" + i }
+                    );
+                    // Zapisanie obiektu
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+
+            // Act
+            var response = await _client.GetAsync("/api/product");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var products = await response.Content.ReadFromJsonAsync<List<Product>>();
+            Assert.Equal(10000, products?.Count);
+        }
     }
 }
