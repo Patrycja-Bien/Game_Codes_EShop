fdde2f7e842cb452abedc90e9743d1dadbeaf982
Patrycja-Bien <232039@student.uek.krakow.pl>
2025-05-09 14:11:48 +0200

    Docker and JWT fix

---
+++ User_Application/Services/IJwtTokenService.cs
@@ -0,0 +1,7 @@
+﻿namespace User_Application.Services;
+
+public interface IJwtTokenService
+{
+    public string GenerateToken(int userId, List<string> roles);
+
+}
---
+++ User_Application/Services/ILoginService.cs
@@ -0,0 +1,6 @@
+﻿namespace User_Application.Services;
+
+public interface ILoginService
+{
+    string Login(string username, string password);
+}
